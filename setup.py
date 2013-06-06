33ec77c892d1f6d43f39d480400525a2bf843a0c
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-06 21:18:06 +0200

    Bump to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tdaemon",
-    version="0.1.2",
+    version="0.1.3",
     maintainer="John Paulett",
     maintainer_email="john@7oars.com",
     description="Test Daemon",

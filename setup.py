1282936f11ddebc8030ecbc716c131b7f0a8dff8
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-06 04:34:59 +0200

    cosmetics

--- setup.py
+++ setup.py
@@ -1,34 +1,29 @@
 from setuptools import setup, find_packages
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 setup(
-    name = "tdaemon",
-    version = "0.1.1",
-    maintainer = "John Paulett",
-    maintainer_email = "john@7oars.com",
-    description = "Test Daemon",
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    url = "http://github.com/brunobord/tdaemon",
-    license = "MIT",
-    platforms = ['POSIX', 'Windows'],
-    keywords = ['test', 'testing', 'noestests', 'django', 'py.test'],
-    packages = find_packages(),
-    data_files = [('', ['LICENSE'])],
-    classifiers = [
+    name="tdaemon",
+    version="0.1.1",
+    maintainer="John Paulett",
+    maintainer_email="john@7oars.com",
+    description="Test Daemon",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    url="http://github.com/brunobord/tdaemon",
+    license="MIT",
+    platforms=['POSIX', 'Windows'],
+    keywords=['test', 'testing', 'noestests', 'django', 'py.test'],
+    packages=find_packages(),
+    data_files=[('', ['LICENSE'])],
+    classifiers=[
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Testing",
         "Programming Language :: Python",
         "Intended Audience :: Developers",
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'tdaemon = tdaemon:main',
         ],
     },
-    py_modules = ['tdaemon'],
-    include_package_data = True,
+    py_modules=['tdaemon'],
+    include_package_data=True,
 )
-
